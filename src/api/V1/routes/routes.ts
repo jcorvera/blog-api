56ee3b6fb60b841a4073820ea41847448b7aa1c9
jcorvera <javier.corvra@gmail.com>
2021-04-03 14:32:12 -0600

    Add Routes for comments

--- src/api/V1/routes/routes.ts
+++ src/api/V1/routes/routes.ts
@@ -4,6 +4,7 @@ import { storePostRequest } from '../validators/post/StorePostRequest';
 const postController = new PostController();
 
 export const routes: RouterInterface[] = [
+  // POSTS ROUTES
   {
     method: 'POST',
     path: '/api/v1/posts',
@@ -18,6 +19,23 @@ export const routes: RouterInterface[] = [
   {
     method: 'DELETE',
     path: /\/api\/v1\/posts\/([0-9a-z]+)/,
+    controller: postController.destroy.bind(postController)
+  },
+  // COMMENT POSTS
+  {
+    method: 'POST',
+    path: /\/api\/v1\/posts\/([0-9a-z]+)\/comments/,
+    controller: postController.store.bind(postController),
+    validate: storePostRequest
+  },
+  {
+    method: 'GET',
+    path: /\/api\/v1\/posts\/([0-9a-z]+)\/comments\/([0-9a-z]+)/,
+    controller: postController.show.bind(postController)
+  },
+  {
+    method: 'DELETE',
+    path: /\/api\/v1\/posts\/([0-9a-z]+)\/comments\/([0-9a-z]+)/,
     controller: postController.destroy.bind(postController)
   }
 ];
